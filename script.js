08ef34b988c4e14ee4a56f1042da939467e5a476
pedroaurelli <pedroaureliano.contato@gmail.com>
2022-01-21 14:27:10 -0300

    alterações

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// Utilizando a API https://viacep.com.br/ws/${CEP}/json/
-// crie um formulário onde o usuário pode digitar o cep
-// e o endereço completo é retornado ao clicar em buscar
-
 const btnEnviar = document.querySelector('.botaoEnviar')
 
 function handleCEP(){
